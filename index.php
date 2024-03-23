7b03878d9397edf768021efb53133dc3b0c3555e
Hafidz Firdaus <hafidzfirdaus633@gmail.com>
2024-03-23 20:46:43 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, dolore aliquam, ullam unde doloribus quos perspiciatis maiores odit itaque sint atque tempora suscipit ipsam veritatis eveniet laborum voluptate excepturi perferendis.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
